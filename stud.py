6695b8a643891eea6c669230ef5e2fb96cdef1c4
MiXACT <out_lawz@mail.ru>
2021-09-19 17:20:16 +0300

    Перегрузка метода __str__

--- stud.py
+++ stud.py
@@ -10,6 +10,18 @@ class Student:
 	def add_courses(self, course_name):
 		self.finished_courses.append(course_name)
 
+	def __str__(self):
+		return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: {self.hw_avrg()}\nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}'
+
+	def hw_avrg(self):
+		average = 0
+		n = 0
+		for val in self.grades.values():
+			for i in range(len(val)):
+				average += int(val[i])
+				n += 1
+		return round(average/n, 1)
+
 	def rate_lect(self, lecturer, course, grade):
 		if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
 			if course in lecturer.lect_grades:
@@ -34,6 +46,18 @@ class Lecturer(Mentor):
 		self.courses_attached = []
 		self.lect_grades = {}
 
+	def __str__(self):
+		return f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average()}'
+
+	def average(self):
+		average = 0
+		n = 0
+		for val in self.lect_grades.values():
+			for i in range(len(val)):
+				average += int(val[i])
+				n += 1
+		return round(average/n, 1)
+
 
 class Reviewer(Mentor):
 	def rate_hw(self, student, course, grade):
@@ -44,16 +68,27 @@ class Reviewer(Mentor):
 				student.grades[course] = [grade]
 		else:
 			return 'Ошибка'
+
+	def __str__(self):
+		return f'Имя: {self.name}\nФамилия: {self.surname}'
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python', 'Scotch try outs', 'Math']
+best_student_2 = Student('Rick', 'Mortynov', 'male')
+best_student_2.courses_in_progress += ['Math']
+best_student_2.add_courses('Python')
  
 cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_mentor.courses_attached += ['Python','Math']
  
 cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 8)
+
+cool_mentor.rate_hw(best_student_2, 'Math', 3)
+cool_mentor.rate_hw(best_student_2, 'Math', 7)
+cool_mentor.rate_hw(best_student_2, 'Python', 9)
+cool_mentor.rate_hw(best_student_2, 'Python', 6)
  
 print(best_student.grades)
 
@@ -64,5 +99,17 @@ best_lecturer_2.courses_attached += ['Math']
 
 best_student.rate_lect(best_lecturer_1, 'Scotch try outs', 5)
 best_student.rate_lect(best_lecturer_2, 'Math', 8)
+best_student_2.rate_lect(best_lecturer_1, 'Scotch try outs', 10)
+best_student_2.rate_lect(best_lecturer_2, 'Math', 9)
 
 print(f'{best_lecturer_1.lect_grades}\n{best_lecturer_2.lect_grades}')
+
+#задача №3: перегрузка метода __str__ для "проверяющих" (Reviewer)
+print(cool_mentor)
+
+#задача №3: перегрузка метода __str__ для лекторов (Lecturer)
+print(best_lecturer_1)
+print(best_lecturer_2)
+
+#задача №3: перегрузка метода __str__ для студентов (Student)
+print(f'{best_student}\n{best_student_2}')
